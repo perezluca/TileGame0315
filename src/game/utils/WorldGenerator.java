e9ce64f7c44d82c5c6feae11de5cc665f3477e8d
perezluca <lperez@student-bmhs-la.org>
2023-03-15 10:21:41 -0700

    day 2

--- src/game/utils/WorldGenerator.java
+++ src/game/utils/WorldGenerator.java
@@ -11,21 +11,28 @@ public class WorldGenerator {
 
     public WorldGenerator(int worldRows, int worldColumns) {
         this.worldRows = worldRows;
+
         this.worldColumns = worldColumns;
 
         worldMap2DArr = new int[worldRows][worldColumns];
+
         setWater();
+
         blackBorder();
+
         dropSeed();
+
         buildIsland();
+
         setLand();
+
         FileGenerator.getFileGenerator().createFile(WORLD_FULL_DIRECTORY_NAME, FileGenerator.getFileGenerator().intArr2DToString(worldMap2DArr));
 
     }
 
     private void setWater() {
-        for(int r= 0; r < worldMap2DArr.length;r++) {
-            for(int c = 0; c < worldMap2DArr[r].length; c++){
+        for (int r = 0; r < worldMap2DArr.length; r++) {
+            for (int c = 0; c < worldMap2DArr[r].length; c++) {
                 worldMap2DArr[r][c] = getWaterIndex();
             }
         }
@@ -35,47 +42,26 @@ public class WorldGenerator {
         for (int r = 0; r < worldMap2DArr.length; r++) {
             for (int c = 0; c < worldMap2DArr[r].length; c++) {
                 if (r == 0 || c == 0 || r == worldMap2DArr[r].length - 1 || c == worldMap2DArr[c].length - 1) {
-                    worldMap2DArr[r][c] = 50;
+                    worldMap2DArr[r][c] = 4;
                 }
             }
         }
     }
 
-
-
-
     private void dropSeed() {
-        /*int numOfIslands = (int)(Math.random()*3+1);
-        for(int){
-
-        }*/
-        int rand = (int) (Math.random() * 100);
-        int dropSeedR = (int) ((Math.random() * (50 - 14)) + 14);
-        int dropSeedC = (int) ((Math.random() * (50 - 14)) + 14);
-        worldMap2DArr[dropSeedR][dropSeedC] = 7;
-        if (rand < 50) {
-            int dropSeedR2 = (int) ((Math.random() * (50 - 14)) + 14);
-            int dropSeedC2 = (int) ((Math.random() * (50 - 14)) + 14);
-
-            worldMap2DArr[dropSeedR2][dropSeedC2] = 7;
-        }
+        for( int count = 0; count <= 20; count++);
+        int dropSeedR = (int) ((Math.random() * (9 - 6)) + 6);
+        int dropSeedC = (int) ((Math.random() * (9 - 6)) + 6);
+        worldMap2DArr[dropSeedR][dropSeedC] = 3;
     }
 
-
     private void buildIsland() {
-
         for (int r = 0; r < worldMap2DArr.length; r++) {
             for (int c = 0; c < worldMap2DArr[r].length; c++) {
-                if (worldMap2DArr[r][c] == 8) {
-                    for (int i = -7; i <= 7; i++) {
-                        for (int j = -7; j <= 7; j++) {
-                            if (j > -4 && j < 4 && i > -4 && i < 4) {
-                                if ((Math.random() < .99)) {
-                                    worldMap2DArr[r + i][c + j] = 0;
-                                }
-                            }
-                            else {
-                                if ((Math.random() < .60)) {
+                if (worldMap2DArr[r][c] == 3) {
+                    for (int i = -1; i <= 1; i++) {
+                        for (int j = -1; j <= 1; j++) {
+                                if (worldMap2DArr[r + i][c + j] != 3) {
                                     worldMap2DArr[r + i][c + j] = 0;
                                 }
                             }
@@ -84,23 +70,22 @@ public class WorldGenerator {
                 }
             }
         }
-    }
 
     private void setLand() {
-        for(int r= 0; r < worldMap2DArr.length;r++) {
-            for(int c = 0; c < worldMap2DArr[r].length; c++){
+        for (int r = 0; r < worldMap2DArr.length; r++) {
+            for (int c = 0; c < worldMap2DArr[r].length; c++) {
                 if (worldMap2DArr[r][c] == 0)
                     worldMap2DArr[r][c] = getLandIndex();
             }
         }
     }
+
     private int getLandIndex() {
-        return (int) ((Math.random() * (77 - 61)) + 60);
+        return (int) ((Math.random() * (10 - 6)) + 6);
     }
 
-
-    private int getWaterIndex(){
-        return (int) ((Math.random() * (62 - 61)) + 61);
+    private int getWaterIndex() {
+        return (int) ((Math.random() * (9 - 7)) + 7);
     }
 
     public String getWORLD_FULL_DIRECTORY_NAME() {
--- src/gfx/utils/ImageHandler.java
+++ src/gfx/utils/ImageHandler.java
@@ -34,71 +34,43 @@ public class ImageHandler {
 
      */
     private void createWorldTiles() {
-        BufferedImage primaryColorSheet = ImageLoader.loadImage("/primary color spritesheet2.png");
+        BufferedImage primaryColorSheet = ImageLoader.loadImage("/primary color spritesheet.png");
 
         BufferedImage clear = primaryColorSheet.getSubimage(0, 0, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
         WorldTile clearTile = new WorldTile(clear, 1);
         setWorldTile(clearTile);
 
-        BufferedImage white = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH, 0, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile whiteTile = new WorldTile(white, 0);
+        BufferedImage white = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH*1, 0, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
+        WorldTile whiteTile = new WorldTile(white, 2);
         setWorldTile(whiteTile);
 
-        BufferedImage grey_0 = primaryColorSheet.getSubimage(0, SPRITE_TILE_HEIGHT, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile grey_0Tile = new WorldTile(grey_0, 35);
-        setWorldTile(grey_0Tile);
-
-        BufferedImage grey_1 = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile grey_1Tile = new WorldTile(grey_1, 11);
-        setWorldTile(grey_1Tile);
-
-        BufferedImage grey_2 = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH*2, SPRITE_TILE_HEIGHT, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile grey_2Tile = new WorldTile(grey_2, 22);
-        setWorldTile(grey_2Tile);
-
-        BufferedImage grey_3 = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH*3, SPRITE_TILE_HEIGHT, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile grey_3Tile = new WorldTile(grey_3, 33);
-        setWorldTile(grey_3Tile);
-
-        BufferedImage grey_4 = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH*4, SPRITE_TILE_HEIGHT, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile grey_4Tile = new WorldTile(grey_4, 4);
-        setWorldTile(grey_4Tile);
-
-        BufferedImage grey_5 = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH*5, SPRITE_TILE_HEIGHT, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile grey_5Tile = new WorldTile(grey_5, 5);
-        setWorldTile(grey_5Tile);
-
-        BufferedImage grey_6 = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH*6, SPRITE_TILE_HEIGHT, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile grey_6Tile = new WorldTile(grey_6, 6);
-        setWorldTile(grey_6Tile);
-
-        BufferedImage grey_7 = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH*7, SPRITE_TILE_HEIGHT, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile grey_7Tile = new WorldTile(grey_7, 7);
-        setWorldTile(grey_7Tile);
-
         BufferedImage red = primaryColorSheet.getSubimage(0, SPRITE_TILE_HEIGHT*2, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile redTile = new WorldTile(red, 1000);
+        WorldTile redTile = new WorldTile(red, 3);
         setWorldTile(redTile);
 
         BufferedImage black = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH*2,0, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile blackTile = new WorldTile(black,50 );
+        WorldTile blackTile = new WorldTile(black,4 );
         setWorldTile(blackTile);
 
-        BufferedImage lightGreen = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT*6, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile lightGreenTile = new WorldTile(lightGreen, 77);
+        BufferedImage lightGreen = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH*1, SPRITE_TILE_HEIGHT*5, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
+        WorldTile lightGreenTile = new WorldTile(lightGreen, 5);
         setWorldTile(lightGreenTile);
 
         BufferedImage darkGreen = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH*2, SPRITE_TILE_HEIGHT*6, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile darkGreenTile = new WorldTile(darkGreen, 60);
+        WorldTile darkGreenTile = new WorldTile(darkGreen, 6);
         setWorldTile(darkGreenTile);
 
-        BufferedImage turquoise = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT*6, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile turquoiseTile = new WorldTile(turquoise, 61);
-        setWorldTile(turquoiseTile);
+        BufferedImage lightBlue = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH*1, SPRITE_TILE_HEIGHT*7, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
+        WorldTile lightBlueTile = new WorldTile(lightBlue, 7);
+        setWorldTile(lightBlueTile);
 
-        BufferedImage blue = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT*7, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
-        WorldTile blueTile = new WorldTile(blue, 62);
+        BufferedImage blue = primaryColorSheet.getSubimage(0, SPRITE_TILE_HEIGHT*7, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
+        WorldTile blueTile = new WorldTile(blue, 8);
         setWorldTile(blueTile);
+
+        BufferedImage tan = primaryColorSheet.getSubimage(SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT*3, SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT);
+        WorldTile tanTile = new WorldTile(tan, 9);
+        setWorldTile(tanTile);
     }
 
     private void setWorldTile(WorldTile tile) {
